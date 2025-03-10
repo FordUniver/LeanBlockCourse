f090084021f32a5032bcfcb0683e7511d7dadc28
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2025-03-10 11:51:18 +0100

    added topology example and slides

--- LeanBlockCourse.lean
+++ LeanBlockCourse.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `LeanBlockCourse` library.
 -- Import modules here that should be built as part of the library.
-import LeanBlockCourse.Basic
+import LeanBlockCourse.P01_Introduction.S01_TopologyExample
---
+++ LeanBlockCourse/P01_Introduction/S01_TopologyExample.lean
@@ -0,0 +1,46 @@
+import Mathlib.Topology.Basic
+
+variable {X Y Z : Type*}
+variable [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace Z]
+
+
+example {f : X → Y} {g: Y → Z}
+        (hf : Continuous f) (hg : Continuous g) :
+        Continuous (g ∘ f) := by
+
+  constructor
+  intros U hU
+
+  let V := g⁻¹' U
+  have hV : IsOpen V := hg.isOpen_preimage U hU
+
+  let W := f⁻¹' V
+  have hW : IsOpen W := hf.isOpen_preimage V hV
+
+  have : W = (g ∘ f)⁻¹' U := rfl  
+  rw [← this]
+
+  exact hW  
+
+example {f : X → Y} {g: Y → Z}
+        (hf : Continuous f) (hg : Continuous g) :
+        Continuous (g ∘ f) := by
+  constructor
+  intros U hU
+
+  have hV : IsOpen (g⁻¹' U) := hg.isOpen_preimage U hU
+  have hW : IsOpen (f⁻¹' (g⁻¹' U)) := hf.isOpen_preimage (g⁻¹' U) hV
+
+  exact hW  
+  
+example {f : X → Y} {g: Y → Z}
+        (hf : Continuous f) (hg : Continuous g) :
+        Continuous (g ∘ f) := by
+  constructor
+  intros U hU
+  exact hf.isOpen_preimage (g⁻¹' U) (hg.isOpen_preimage U hU)
+
+example {f : X → Y} {g: Y → Z}
+        (hf : Continuous f) (hg : Continuous g) :
+        Continuous (g ∘ f) :=
+  { isOpen_preimage := λ U hU => hf.isOpen_preimage (g⁻¹' U) (hg.isOpen_preimage U hU) }
