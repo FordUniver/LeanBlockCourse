6f8bbb90e8e6a90a5428001c8f29f5c86a2f13a1
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2025-03-12 14:38:50 +0100

    completed quantifiers

--- LeanBlockCourse/P02_Logic/S05_Quantifiers.lean
+++ LeanBlockCourse/P02_Logic/S05_Quantifiers.lean
@@ -90,8 +90,6 @@ example : ∀ (X : Type) (P : X → Prop),
   obtain ⟨x, h_existence, h_uniqueness⟩ := h
   exact ⟨x, h_existence⟩
 
-#check ExistsUnique 
-
 example : ∀ (X : Type) (P : X → Prop),
   (∃! (x : X), P x) → ∃ (x : X), P x := 
   fun _ _ ⟨x, h_existence, _⟩ => Exists.intro x h_existence
@@ -126,60 +124,132 @@ example (X Y : Type) (f g : X → Y) (h : ∀ x : X, f x = g x) : f = g := by
 
 -- Prove that `∀ x, (p x ∧ q x) ↔ ((∀ x, p x) ∧ (∀ x, q x))`.
 example (α : Type) (p q : α → Prop) : (∀ x : α, p x ∧ q x) ↔ ((∀ x : α, p x) ∧ (∀ x : α, q x)) := by
-  sorry
+  constructor
+  · intro h
+    constructor
+    · intro x
+      obtain ⟨px, qx⟩ := h x
+      exact px
+    · intro x
+      obtain ⟨px, qx⟩ := h x
+      exact qx
+  · intro ⟨h₁, h₂⟩
+    intro x
+    have px := h₁ x
+    have qx := h₂ x
+    exact ⟨px, qx⟩
+
+example (α : Type) (p q : α → Prop) : (∀ x : α, p x ∧ q x) ↔ ((∀ x : α, p x) ∧ (∀ x : α, q x)) :=
+  ⟨fun h => ⟨fun x => (h x).1, fun x => (h x).2⟩, fun ⟨h₁, h₂⟩ x => ⟨h₁ x, h₂ x⟩⟩ 
+
+example (α : Type) (p q : α → Prop) : (∀ x : α, p x ∧ q x) ↔ ((∀ x : α, p x) ∧ (∀ x : α, q x)) :=
+  forall_and -- found through `exact?`
 
 -- Prove that `∃ x, (p x ∨ q x) ↔ ((∃ x, p x) ∨ (∃ x, q x))`.
 example (α : Type) (p q : α → Prop) : (∃ x : α, p x ∨ q x) ↔ ((∃ x : α, p x) ∨ (∃ x : α, q x)) := by
-  sorry
+  constructor
+  · intro h
+    obtain ⟨x, (px | qx)⟩ := h
+    · left; use x
+    · right; use x
+  · intro h
+    obtain (⟨x, px⟩ | ⟨x, qx⟩) := h
+    · use x; left; exact px
+    · use x; right; exact qx 
+
+example (α : Type) (p q : α → Prop) : (∃ x : α, p x ∨ q x) ↔ ((∃ x : α, p x) ∨ (∃ x : α, q x)) :=
+  exists_or -- found through `exact?`
 
 -- Prove that `((∀ x, p x) ∨ (∀ x, q x)) → (∀ x, p x ∨ q x)`.
 example (α : Type) (p q : α → Prop) : ((∀ x : α, p x) ∨ (∀ x : α, q x)) → (∀ x : α, p x ∨ q x) := by
-  sorry
+  intro h x
+  obtain (px | qx) := h
+  · left; exact px x
+  · right; exact qx x
 
--- Use the `ext` tactic to prove the following:
-example (X : Type) (A B : Set X) (h : ∀ x, x ∈ A ↔ x ∈ B) : A = B := by
-  sorry
-
--- Use the `use` tactic to prove the following:
-example (X Y : Type) (P : X → Prop) (Q : Y → Prop)
-  (h₁ : ∃ x, P x) (h₂ : ∃ y, Q y) : ∃ z : X × Y, P z.1 ∧ Q z.2 := by
-  sorry
+-- Prove that `(∃ x, P x ∧ Q x) → (∃ x, P x) ∧ (∃ x, Q x)`.
+example (α : Type) (P Q : α → Prop) : (∃ x, P x ∧ Q x) → (∃ x, P x) ∧ (∃ x, Q x) := by
+  intro ⟨x, px, qx⟩
+  constructor
+  all_goals use x
 
 -- Use the `choose` tactic to prove the following:
 example (X : Type) (R : X → X → Prop) (h : ∀ x, ∃ y, R x y) :
     ∃ f : X → X, ∀ x, R x (f x) := by
-  sorry
+  choose f hf using h
+  use f
 
--- Prove that `(∃ x, P x ∧ Q x) → (∃ x, P x) ∧ (∃ x, Q x)`.
-example (α : Type) (P Q : α → Prop) : (∃ x, P x ∧ Q x) → (∃ x, P x) ∧ (∃ x, Q x) := by
-  sorry
+example (X : Type) (R : X → X → Prop) (h : ∀ x, ∃ y, R x y) :
+    ∃ f : X → X, ∀ x, R x (f x) :=
+  (fun f hf => Exists.intro f hf) (fun x => Classical.choose (h x)) fun x => Classical.choose_spec (h x)
+
+-- Use the `use` tactic to prove the following:
+example (X Y : Type) (P : X → Prop) (Q : Y → Prop)
+  (h₁ : ∃ x, P x) (h₂ : ∃ y, Q y) : ∃ z : X × Y, P z.1 ∧ Q z.2 := by
+  obtain ⟨x, px⟩ := h₁
+  obtain ⟨y, qy⟩ := h₂
+  use ⟨x, y⟩
 
 /-
 ## Remark
 
-Looking at mathlib, you will find the following definitions:
-
+Looking at mathlib, you will find `∃` is defined as follows:
 
 inductive Exists {α : Sort u} (p : α → Prop) : Prop where
   | intro (w : α) (h : p w) : Exists p
 
-def ExistsUnique (p : α → Prop) := ∃ x, p x ∧ ∀ y, p y → y = x
+`∃!` is defined as a predicate using `∃`:
 
-For ∀ you will not find such a clear definition since it is notation.
-However the following shows that we essentially have three ways of expressing
-the same:
+def ExistsUnique (p : α → Prop) := ∃ x, p x ∧ ∀ y, p y → y = x
 -/
+
+#check Exists
+
+-- `Exists` in the goal can be closed through `use` ...
+example : ∃ n, n = 2 := by
+  use 2
+
+-- ... or through `exact` with a constructor.
+example : ∃ n, n = 2 := by
+  exact ⟨2, rfl⟩
+
+example : ∃ n, n = 2 := by
+  exact Exists.intro 2 rfl
+
+-- `Exists` in an assumption can be destructured using `obtain`  
+example (X : Type) (P : X → Prop) (h : (∃! (x : X), P x)) : ∃ (x : X), P x := by
+  obtain ⟨x, px, unique_x⟩ := h
+  exact ⟨x, px⟩
+
+
+/-
+For `∀` you will not find such a clear definition since it is notation.
+However the following shows that we essentially have three ways of expressing the same thing:
+-/
+section
 
 variable (X : Type) (P : X → Prop)
 
 axiom f1 (x : X) : P x
 
-axiom f2 : ∀ (x : X), P x
+axiom f2 : (x : X) → P x -- this is what is going on internally in lean
 
-axiom f3 : (x : X) → P x
+axiom f3 : ∀ (x : X), P x
 
 example : f1 = f2 := rfl
 
 example : f1 = f3 := rfl
 
 example : f2 = f3 := rfl
+
+end
+
+
+-- For all in the goal can be therefore dealt with through `intro` ...
+example : ∀ (P : Prop), P ↔ P := by
+  intro P
+  rfl
+
+-- ... while for all in an assumption can be dealt with an application
+example (P : ℕ → Prop) (h : ∀ x, P x) : P 0 :=
+  by exact h 0
