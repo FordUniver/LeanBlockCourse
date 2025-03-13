b9f7686039d791eda65267e12f9a2b3731dbb969
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2025-03-13 12:32:42 +0100

    completed exercises

--- LeanBlockCourse/P03_SetTheory/S02_IntersectionsUnions.lean
+++ LeanBlockCourse/P03_SetTheory/S02_IntersectionsUnions.lean
@@ -5,6 +5,7 @@ https://github.com/leanprover-community/lean4game
 
 import Mathlib.Data.Set.Basic
 import Mathlib.Tactic.Cases
+import Mathlib.Tactic.NthRewrite
 import LeanBlockCourse.P03_SetTheory.S01_SubsetsComplements
 
 variable {α : Type*}
@@ -115,36 +116,120 @@ lemma mem_union (x : α) (S T : Set α) : x ∈ S ∪ T ↔ x ∈ S ∨ x ∈ T 
 
 -- If `x ∈ T`, then `x ∈ S ∪ T`.
 lemma subset_union_right (S T : Set α) : T ⊆ S ∪ T := by
-  sorry
+  intro x xt
+  rw [Set.mem_union]
+  right
+  exact xt
+
+example (S T : Set α) : T ⊆ S ∪ T := fun _ xt => Or.inr xt
 
 -- Prove that if `R ⊆ T` and `S ⊆ T`, then `R ∪ S ⊆ T`.
 lemma union_subset (R S T : Set α) (h₁ : R ⊆ T) (h₂ : S ⊆ T) : R ∪ S ⊆ T := by
-  sorry
+  rintro x (xr | xs)
+  · exact h₁ xr
+  · exact h₂ xs
+
+example (R S T : Set α) (h₁ : R ⊆ T) (h₂ : S ⊆ T) : R ∪ S ⊆ T := by
+  rintro x xrs
+  cases' xrs with xr | xs
+  · exact h₁ xr
+  · exact h₂ xs
 
 -- Prove that `S ∪ T ⊆ T ∪ S`.
 lemma union_subset_swap (S T : Set α) : S ∪ T ⊆ T ∪ S := by
-  sorry
+  intro x xst
+  rw [mem_union] at *
+  apply or_comm.mp 
+  exact xst
+
+example (S T : Set α) : S ∪ T ⊆ T ∪ S := by
+  intro x xst
+  rw [mem_union] at *
+  rw [or_comm]
+  exact xst
+
+example (S T : Set α) : S ∪ T ⊆ T ∪ S := fun _ xst => or_comm.mp xst
 
 -- Prove that `S ∪ T = T ∪ S`.
 lemma union_comm (S T : Set α) : S ∪ T = T ∪ S := by
-  sorry
+  ext x
+  -- rw [mem_union]
+  exact or_comm
 
 -- Prove that the complement of the intersection of two sets is the union of their complements.
 lemma compl_inter (S T : Set α) : (S ∩ T)ᶜ = Sᶜ ∪ Tᶜ := by
-  sorry
+  ext x
+  constructor
+  · intro xstc
+    obtain : x ∈ S ∩ T → False := xstc
+    by_contra xsctc
+    rw [mem_union] at xsctc
+    push_neg at xsctc
+    obtain ⟨xsc, xtc⟩ := xsctc
+    obtain xs : x ∈ S := Set.not_mem_compl_iff.mp xsc
+    obtain xt : x ∈ T := Set.not_mem_compl_iff.mp xtc
+    rw [Set.mem_inter_iff] at xstc
+    exact xstc ⟨xs, xt⟩
+  · intro h
+    by_contra nxstc
+    obtain xst : x ∈ S ∩ T := Set.not_mem_compl_iff.mp nxstc
+    obtain ⟨xs, xt⟩ := xst
+    rw [mem_union] at h
+    obtain (h₁ | h₂) := h
+    · contradiction
+    · contradiction
+
+example (S T : Set α) : (S ∩ T)ᶜ = Sᶜ ∪ Tᶜ := by
+  ext x
+  rw [mem_compl_iff, mem_inter_iff, mem_union, mem_compl_iff, mem_compl_iff]
+  push_neg
+  constructor
+  · intro h
+    by_cases xs : x ∈ S 
+    · right; exact h xs
+    · left; exact xs
+  · intro h xs
+    obtain (nxs | nxt) := h
+    · contradiction
+    · exact nxt
+
 
 -- Prove that the complement of the union of two sets is the intersection of their complements.
 lemma compl_union (S T : Set α) : (S ∪ T)ᶜ = Sᶜ ∩ Tᶜ := by
-  sorry
+  nth_rw 1 [← compl_compl S, ← compl_compl T]
+  rw [← compl_inter Sᶜ Tᶜ, compl_compl]
+  -- This nice proof is thanks to Silas!
 
 -- Prove that the union of three sets is associative.
 lemma union_assoc (R S T : Set α) : (R ∪ S) ∪ T = R ∪ (S ∪ T) := by
-  sorry
+  ext x
+  -- rw [mem_union, mem_union, mem_union, mem_union]
+  exact or_assoc
 
 -- Prove that the intersection of a set with the union of two sets is distributive.
 lemma inter_distrib_left (R S T : Set α) : R ∩ (S ∪ T) = (R ∩ S) ∪ (R ∩ T) := by
-  sorry
+  ext x
+  -- rw [mem_union, mem_inter_iff, mem_union, mem_inter_iff, mem_inter_iff]
+  exact and_or_left -- this is just `P ∧ (Q ∨ R) ↔ (P ∧ Q) ∨ (P ∧ R)`
+
+-- Note the inconsistent naming scheme
+#check Set.mem_union
+#check Set.mem_inter_iff 
 
 -- Prove that if `R ∪ T ⊆ S ∪ T` and `R ∩ T ⊆ S ∩ T`, then `R ⊆ S`. 
 example (R S T : Set α) (h₁ : R ∪ T ⊆ S ∪ T) (h₂ : R ∩ T ⊆ S ∩ T) : R ⊆ S := by
-  sorry
+  intro x xr
+  have xrut : x ∈ R ∪ T := Or.inl xr
+  have xsut : x ∈ S ∪ T := h₁ xrut
+  obtain (xs | xt) := xsut
+  · assumption
+  · have xrit : x ∈ R ∩ T := ⟨xr, xt⟩
+    have xsit : x ∈ S ∩ T := h₂ xrit
+    obtain ⟨xs, xt⟩ := xsit
+    assumption
+
+example (R S T : Set α) (h₁ : R ∪ T ⊆ S ∪ T) (h₂ : R ∩ T ⊆ S ∩ T) : R ⊆ S := by
+  intro x xr
+  obtain (xs | xt) := h₁ (Or.inl xr)
+  · exact xs
+  · exact (h₂ ⟨xr, xt⟩).1
