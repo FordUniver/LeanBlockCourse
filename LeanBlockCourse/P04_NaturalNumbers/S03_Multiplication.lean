cbc8b4c09cec6d9740c7e307d46edca440125f10
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2025-03-14 14:34:44 +0100

    added mul

---
+++ LeanBlockCourse/P04_NaturalNumbers/S03_Multiplication.lean
@@ -0,0 +1,21 @@
+/-
+This part is mostly inspired by the `Natural Number Game`:
+https://adam.math.hhu.de/#/g/leanprover-community/nng4
+-/
+
+import BlockCourse.P05_NaturalNumbers.S02_Addition
+import Mathlib.Tactic.ByContra
+
+namespace MyNat
+
+/-
+## Exercises: Define Multiplication Inductively
+-/
+
+-- Define multiplication `mul` inductively
+
+-- show that we have an instance of `instMul`
+
+-- show that `n * 0 = 0`
+
+-- show that `n * succ m = n * m + n`
