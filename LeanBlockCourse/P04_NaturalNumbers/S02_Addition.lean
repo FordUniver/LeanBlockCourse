40ae910d9ed18048ac5d2134fffe99f503fdd264
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2025-03-14 11:58:53 +0100

    added exercises for inductively defined addition

--- LeanBlockCourse/P04_NaturalNumbers/S02_Addition.lean
+++ LeanBlockCourse/P04_NaturalNumbers/S02_Addition.lean
@@ -28,16 +28,55 @@ axiom add_succ' (a b : MyNat) : add' a (succ b) = succ (add' a b)
 
 -- x + 2 = x + 2
 example (x : MyNat) : add' x 2 = add' x 2 := by
-  sorry
+  rfl
 
 -- a + (b + 0) + (c + 0) = a + b + c
 example (a b c : MyNat) : add' a (add' (add' b 0) (add' c 0)) = add' a (add' b c) := by
-  sorry
+  rw [add_zero', add_zero']
 
 -- succ n = n + 1
 lemma succ_eq_add_one' (n : MyNat) : succ n = add' n 1 := by
-  sorry
+  rw [one_eq_succ_zero]
+  rw [add_succ']
+  rw [add_zero']
 
 -- 2 + 2 = 4
 example : add' 2 2 = 4 := by
+  nth_rw 2 [two_eq_succ_one]
+  rw [add_succ']
+  rw [one_eq_succ_zero]
+  rw [add_succ']
+  rw [add_zero']
+  rfl
+
+
+/-
+But shouldn't all of these be *definitionally* equal? Something is off ...
+
+## Defining Addition: Attempt #2
+-/
+
+def add (a b : MyNat) : MyNat :=
+  match b with 
+  | MyNat.zero => a                -- same as `axiom add_zero'`
+  | MyNat.succ d => (add a d).succ -- same as `axiom add_succ'`
+
+/-
+## Exercises
+-/
+
+-- x + 2 = x + 2
+example (x : MyNat) : add x 2 = add x 2 := by
+  sorry
+
+-- a + (b + 0) + (c + 0) = a + b + c
+example (a b c : MyNat) : add a (add (add b 0) (add c 0)) = add a (add b c) := by
+  sorry
+
+-- succ n = n + 1
+lemma succ_eq_add_one (n : MyNat) : succ n = add n 1 := by
+  sorry
+
+-- 2 + 2 = 4
+example : add 2 2 = 4 := by
   sorry
