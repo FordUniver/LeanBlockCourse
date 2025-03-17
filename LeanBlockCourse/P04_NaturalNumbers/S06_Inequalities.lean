6f58bed05793d1aae4d89f7c547e97c194ff7ce0
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2025-03-17 12:07:24 +0100

    added exercise solutions

--- LeanBlockCourse/P04_NaturalNumbers/S06_Inequalities.lean
+++ LeanBlockCourse/P04_NaturalNumbers/S06_Inequalities.lean
@@ -26,8 +26,9 @@ lemma le_refl (n : MyNat) : n ≤ n := by
 
 lemma zero_le (n : MyNat) : 0 ≤ n := by
   use n
-  rw [zero_add] 
+  rw [zero_add]
 
+lemma le_iff (n m : MyNat) : n ≤ m ↔ ∃ k, m = n + k := by rfl 
 
 /-
 We also define `m < n` as `m ≤ n ∧ m ≠ n` OR as `m + 1 ≤ n`
@@ -43,24 +44,6 @@ lemma zero_lt (n : MyNat) (h : n ≠ 0) : 0 < n := by
   · use n'
     rw [add_comm]
     rfl
-
-lemma lt_iff_le_ne (m n : MyNat) : m < n ↔ m ≤ n ∧ m ≠ n := by
-  constructor
-  · intro ⟨k, hk⟩
-    rw [succ_add] at hk
-    constructor
-    · use k.succ
-      exact hk
-    · by_contra hc
-      rw [hc] at hk
-      replace hk : 0 = k.succ := (add_right_eq_self hk.symm).symm
-      contradiction
-  · intro ⟨⟨k, hk⟩, h⟩
-    cases' k with k'
-    · symm at hk; contradiction
-    · use k'
-      rw [succ_add]
-      assumption
 
 lemma lt_iff_le_ne' (m n : MyNat) : m < n ↔ ∃k, k ≠ 0 ∧ m + k = n := by
   constructor
@@ -82,31 +65,88 @@ lemma lt_iff_le_ne' (m n : MyNat) : m < n ↔ ∃k, k ≠ 0 ∧ m + k = n := by
 -/
 
 lemma le_succ_self (n : MyNat) : n ≤ succ n := by
-  sorry
+  -- rw [le_iff]
+  use 1
+  rfl 
   
 lemma le_trans {n m k : MyNat} (hnm : n ≤ m) (hmk : m ≤ k) : n ≤ k := by
-  sorry
+  rw [le_iff] at *
+  obtain ⟨x, hx⟩ := hnm
+  obtain ⟨y, hy⟩ := hmk
+  rw [hy, hx]
+  use x + y
+  rw [add_assoc]
 
 lemma le_zero {n : MyNat} (hn : n ≤ 0) : n = 0 := by
-  sorry
+  obtain ⟨k, hk⟩ := hn
+  exact add_right_eq_zero hk.symm
 
 lemma le_antisymm (n m : MyNat) (hnm : n ≤ m) (hmn : m ≤ n) : n = m := by
-  sorry
+  obtain ⟨x, hx⟩ := hnm
+  obtain ⟨y, hy⟩ := hmn
+  rw [hx]
+  rw [hx, add_assoc] at hy
+  replace hy := add_right_eq_self hy.symm
+  have := add_right_eq_zero hy
+  rw [this, add_zero]
 
 lemma succ_le_succ {n m : MyNat} (hn : succ n ≤ succ m) : n ≤ m := by
-  sorry
+  obtain ⟨x, hx⟩ := hn
+  use x
+  apply succ_inj
+  rw [succ_add] at hx
+  assumption 
 
 example (n m : MyNat) (h : n = 37 ∨ m = 42) : m = 42 ∨ n = 37 := by
-  sorry
+  cases h
+  · right; assumption
+  · left; assumption
 
 lemma le_one {n : MyNat} (hn : n ≤ 1) : n = 0 ∨ n = 1 := by
-  sorry
+  cases' n with n'
+  · left; rfl 
+  · right
+    obtain ⟨x, hx⟩ := hn
+    rw [one_eq_succ_zero, succ_add] at hx
+    replace hx := succ_inj hx
+    rw [add_right_eq_zero hx.symm]
+    rfl 
 
-lemma lt_iff_le_and_ne (n m : MyNat) : n < m ↔ n ≤ m ∧ n ≠ m := by
-  sorry
+lemma lt_iff_le_and_ne (m n : MyNat) : m < n ↔ m ≤ n ∧ m ≠ n := by
+  constructor
+  · intro ⟨k, hk⟩
+    rw [succ_add] at hk
+    constructor
+    · use k.succ
+      exact hk
+    · by_contra hc
+      rw [hc] at hk
+      replace hk : 0 = k.succ := (add_right_eq_self hk.symm).symm
+      contradiction
+  · intro ⟨⟨k, hk⟩, h⟩
+    cases' k with k'
+    · symm at hk; contradiction
+    · use k'
+      rw [succ_add]
+      assumption
 
 lemma le_total (n m : MyNat) : n ≤ m ∨ m ≤ n := by
-  sorry
+  induction' m with m' ih
+  · right; rw [← zero_eq_zero]; exact zero_le n
+  · obtain (⟨x, hx⟩ | ⟨x, hx⟩) := ih
+    · left
+      use x + 1
+      rw [← succ_eq_add_one, add_succ, hx] 
+    · cases' x with x'
+      · rw [← zero_eq_zero, add_zero] at hx
+        left
+        rw [hx]
+        exact le_succ_self m'
+      · right
+        use x'
+        rw [add_succ] at hx
+        rw [succ_add]
+        assumption
 
 
 /-
@@ -115,8 +155,12 @@ lemma le_total (n m : MyNat) : n ≤ m ∨ m ≤ n := by
 Fermat's Last Theorem states that if `x, y, z > 0` and
 `m ≥ 3` then `x^m + y^m ≠ z^m`.
 
-Te shortest solution known to humans would translate into
+The shortest solution known to humans would translate into
 many millions of lines of Lean code. Kevin Buzzard is working
 on translating the proof by Wiles and Taylor into Lean, although
 this task will take many years.
 -/
+
+theorem fermats_last_theorem (x y z m : MyNat)
+  (hm : m ≥ 3) (hx : x > 0) (hy : y > 0) (hz : z > 0) :
+  x^m + y^m ≠ z^m := by sorry
