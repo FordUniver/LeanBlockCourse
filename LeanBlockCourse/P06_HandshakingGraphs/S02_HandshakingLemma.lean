0c24dbac8046d55d4352ab3e5f99554a31c177e6
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2025-03-19 16:24:00 +0100

    state theorem

---
+++ LeanBlockCourse/P06_HandshakingGraphs/S02_HandshakingLemma.lean
@@ -0,0 +1,12 @@
+import Mathlib.Combinatorics.SimpleGraph.Basic
+import Mathlib.Combinatorics.SimpleGraph.Finite
+import LeanBlockCourse.ToMathlib.EdgeFinset
+
+/-
+State the following theorem in Lean:
+
+The sum of the degrees of a finite graph is equal
+to twice the number of edges.
+-/
+
+-- lemma handshaking_lemma
