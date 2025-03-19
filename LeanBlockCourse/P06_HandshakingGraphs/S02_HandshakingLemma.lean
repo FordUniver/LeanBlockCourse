6e4d1f2d4aef4527b61eaeb97f2d9c2417f14073
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2025-03-19 16:44:10 +0100

    added handshaking lemma statement

--- LeanBlockCourse/P06_HandshakingGraphs/S02_HandshakingLemma.lean
+++ LeanBlockCourse/P06_HandshakingGraphs/S02_HandshakingLemma.lean
@@ -9,4 +9,49 @@ The sum of the degrees of a finite graph is equal
 to twice the number of edges.
 -/
 
--- lemma handshaking_lemma
+variable {V : Type*} [Fintype V]
+variable (G : SimpleGraph V) [DecidableRel G.Adj]
+
+local notation "E" => G.edgeFinset
+local notation "d(" v ")" => G.degree v
+local notation "I(" v ")" => G.incidenceFinset v
+local notation "N(" v ")" => G.neighborFinset v
+
+open Finset
+
+-- Remark: `∑ v` or `∑ v : V` but NOT `∑ v ∈ V` (`V` is a type!)
+lemma handshaking : ∑ v, d(v) = 2 * #E := by
+  sorry
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+variable {α : Type*} [Fintype α]
+variable (G : SimpleGraph α) [DecidableRel G.Adj]
+
+local notation "E" => G.edgeFinset
+local notation "d(" v ")" => G.degree v
+local notation "I(" v ")" => G.incidenceFinset v
+local notation "N(" v ")" => G.neighborFinset v
+
+open Finset
+
+lemma handshaking : ∑ v, d(v) = 2 * #E := by sorry
