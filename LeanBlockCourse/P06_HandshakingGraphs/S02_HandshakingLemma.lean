1642db13b2436765e940e5334b8d09246f436ba2
Christoph Spiegel <christophspiegel.berlin@gmail.comm>
2025-03-20 10:28:47 +0100

    added definition of double counting set

--- LeanBlockCourse/P06_HandshakingGraphs/S02_HandshakingLemma.lean
+++ LeanBlockCourse/P06_HandshakingGraphs/S02_HandshakingLemma.lean
@@ -1,6 +1,7 @@
-import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Combinatorics.SimpleGraph.Finite
+import Mathlib.Algebra.Order.BigOperators.Ring.Finset
 import LeanBlockCourse.ToMathlib.EdgeFinset
+import Mathlib.Combinatorics.Enumerative.DoubleCounting
 
 /-
 State the following theorem in Lean:
@@ -9,7 +10,7 @@ The sum of the degrees of a finite graph is equal
 to twice the number of edges.
 -/
 
-variable {V : Type*} [Fintype V]
+variable {V : Type*} [Fintype V] [DecidableEq V]
 variable (G : SimpleGraph V) [DecidableRel G.Adj]
 
 local notation "E" => G.edgeFinset
@@ -21,37 +22,5 @@ open Finset
 
 -- Remark: `∑ v` or `∑ v : V` but NOT `∑ v ∈ V` (`V` is a type!)
 lemma handshaking : ∑ v, d(v) = 2 * #E := by
+  let T := {(v, e) : V × (Sym2 V) | e ∈ E ∧ v ∈ e }.toFinset
   sorry
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-variable {α : Type*} [Fintype α]
-variable (G : SimpleGraph α) [DecidableRel G.Adj]
-
-local notation "E" => G.edgeFinset
-local notation "d(" v ")" => G.degree v
-local notation "I(" v ")" => G.incidenceFinset v
-local notation "N(" v ")" => G.neighborFinset v
-
-open Finset
-
-lemma handshaking : ∑ v, d(v) = 2 * #E := by sorry
